4f147dc20ad7a82ba9226c0f221464aa80c7a50c
Seth Peterson <S-seth.peterson@lwtech.edu>
2020-04-30 05:54:48 -0700

    Added file header and changed content of the file.

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -1,5 +1,11 @@
+/* Behold my file header, it is not that mighty.
+ * Author: Seth Peterson
+ * Date: 2020-04-30
+ * Class: CSD 299
+ * Assignment #2
+ */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! I've been EDITED!");
     }
 }
